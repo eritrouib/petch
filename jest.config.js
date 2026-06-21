248ce261d8c7c173a9321d2e29b5d2e0490ac1bf
eritrouib <48014043+eritrouib@users.noreply.github.com>
2026-06-21 16:19:23 +0100

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,7 @@
-/** @type {import('jest').Config} */
-export default {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+/** @type import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
   },
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
-  },
-  collectCoverageFrom: ["src/**/*.ts"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 90,
-      lines: 90,
-    },
-  },
-};
+})
